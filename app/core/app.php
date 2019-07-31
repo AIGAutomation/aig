50ef26342704377f4b9cacca0f8f52a33182317b
HardCoreWare <bernardocodu@gmail.com>
2019-07-31 15:48:15 -0500

    listo

--- app/core/app.php
+++ app/core/app.php
@@ -31,6 +31,8 @@ $app->get('/api/marca', \App\Controllers\ApiController::class.':marca');
 $app->get('/api/sector', \App\Controllers\ApiController::class.':sector');
 $app->get('/api/medida', \App\Controllers\ApiController::class.':medida');
 $app->get('/api/producto/{marca}/{medida}/{sector}', \App\Controllers\ApiController::class.':producto');
+$app->get('/test', \App\Controllers\AigController::class.':test');
+
 //
 /******************/
 /****EJECUTAMOS****/
